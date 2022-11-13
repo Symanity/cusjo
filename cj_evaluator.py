f3d7362f878dd5041e6002b05c89e38343f02beb
IVIrHAHA <ivirhaha@gmail.com>
2022-11-13 14:26:25 -0800

    duration working

--- cj_evaluator.py
+++ cj_evaluator.py
@@ -1,47 +1,162 @@
-import cj_interpreter as inter
 import json
 import datetime
 from datetime import date 
 
-def evaluate(customerData):
-    customer = json.loads(customerData)
-    if isActive(customer):
-        print(customer["name"], ' => active')
-    else:
-        print(customer["name"], ' => inactive')
+DATE_FORMAT = '%m/%d/%y'
 
-    # --> CONSIDER JOB TYPE <---  For now, base on jobs done within two years
 
-        # DURATION 
+class Evaluator:
+    def __init__(self, customer) -> None:
+        self.customerName = customer["name"]
+        self.evaluatations = Evaluator.__evals__(customer["jobHistory"])
+        
 
-        # FREQUENCY 
+    def __evals__(jobHistory):
+        evalDict = {}
 
-        # CURRENT PRICE 
-    
+        for job in jobHistory:
+            jobType = job["type"]
 
-# DETERMINE IF ACTIVE - Determined based on 2+ years of nothing done
-def isActive(customer, effectiveDays = 730):
-    jobHistory = customer["jobHistory"]
-    dateFormat = '%m/%d/%y'
-    today = date.today()
-    print(today)
+            if not jobType in evalDict:
+                evalDict[jobType] = Evaluation(jobType)
+            
+            evalDict[jobType].jobs.append(job)
 
-    for job in jobHistory:
-        jobDateStr = job["date"]
-        jobDate = datetime.datetime.strptime(jobDateStr, dateFormat).date()
+        return evalDict
 
-        if abs(today-jobDate).days < effectiveDays:
+
+    # Active status if job completed within x amount of days [defaults to 2 years]
+    def isActive(customer, effectiveDays = 730):
+        jobHistory = customer["jobHistory"]
+        dateFormat = '%m/%d/%y'
+        today = date.today()
+
+        for job in jobHistory:
+            jobDateStr = job["date"]
+            jobDate = datetime.datetime.strptime(jobDateStr, dateFormat).date()
+
+            if abs(today-jobDate).days < effectiveDays:
+                return True
+
+        return False
+
+    def __str__(self) -> str:
+        return self.customerName
+
+
+
+class Evaluation:
+    def __init__(self, jobType) -> None:
+        self.jobType = jobType
+        self.jobs = []
+
+
+    def __considerDate(targetDate, startDate, endDate):
+        if targetDate > startDate and targetDate < endDate:
             return True
+        else:
+            return False
 
-    return False
+    def __getJobsInRange(self, startDate, endDate):
+        jobList = []
+        for job in self.jobs:
+            completedDate = datetime.datetime.strptime(job["date"], DATE_FORMAT).date()
+            if Evaluation.__considerDate(completedDate, startDate, endDate):
+                jobList.append(job)
+            
+        return jobList
 
-customers = inter.load()
 
-evaluate(customers[8])
+    # Get avg duration of this job type
+    def getAvgDuration(self, startDate = datetime.datetime.strptime("01/01/86", DATE_FORMAT).date(), 
+                        endDate = date.today()):
+        
+        effectiveJobs = self.__getJobsInRange(startDate, endDate)
 
+        avgDuration = 0
+        jobWithTimes = 0
+
+        for job in effectiveJobs:
+            duration = toMinutes(job["duration"])
+            if duration:
+                jobWithTimes += 1
+                avgDuration += duration
+
+        if jobWithTimes > 0:
+            return avgDuration/jobWithTimes
+        else:
+            return None
+
+
+    # Get frequency of this job type
+    def getFrequency():
+        pass
+
+
+    # Get most recent price
+    def getPrice(self):
+        today = date.today()
+        mostRecent = None
+        
+        for job in self.jobs:
+            jobDate = datetime.datetime.strptime(job["date"], DATE_FORMAT).date()
+
+            if not mostRecent:
+                mostRecent = jobDate
+            
+            
+
+
+# Converts into minutes
+def toMinutes(timeStr):
+    timeDict = {
+        "m": None,
+        "h": None,
+    }
+
+    if timeStr:
+        numStr = ''
+        for c in timeStr:
+            if c >= '0' and c <= '9' or c == '.':
+                numStr += c
+
+            # Store numStr as minute
+            elif c == 'm' or c == 'M':
+                timeDict["m"] = numStr.strip()
+                numStr = ''
+
+            elif c == 'h' or c == 'H':
+                timeDict["h"] = numStr.strip()
+                numStr = ''
+
+    # No time was provided
+    else:
+        timeDict = None
+
+    return dictToMins(timeDict)
+
+
+def dictToMins(timeDict):
+    mins = 0
+    hrs = 0
+
+    if timeDict:
+        try:
+            if(timeDict["m"]):
+                mins = float(timeDict["m"])
+
+            if(timeDict["h"]):
+                hrs = float(timeDict["h"])
+            
+            return mins + (hrs*60)
+        except:
+            print('[FATAL] Error occured converting to decimal')
+            return None
+
+    return None
 
 # OUTPUT:
-#  - KeyID => Avg. Duration, From: xx/xx/xxxx to xx/xx/xxxx
+#  - Name => Avg. Duration, From: xx/xx/xxxx to xx/xx/xxxx
 #     ** Frequency    - at least once every 2 years, 
 #                     - 1x per year
 #                     - 2x per year
---
+++ commercial_job_evaluator.py
@@ -0,0 +1,23 @@
+import cj_interpreter as inter
+import json
+import datetime
+from datetime import date 
+import cj_evaluator as eval
+
+DATE_FORMAT = '%m/%d/%y'
+
+customers = inter.load()
+evals = []
+
+startDate, endDate = datetime.datetime.strptime("01/01/16", DATE_FORMAT).date(), date.today()
+
+evaluation = eval.Evaluator(json.loads(customers[14]))
+print('LOOKING AT ', evaluation.customerName)
+# evals.append(evaluation)
+
+evals = evaluation.evaluatations
+for evalKey in evals:
+    duration = evals[evalKey].getAvgDuration(startDate, endDate)
+    print(evalKey, "took an avg of", str(duration), 'mins')
+
+# print(evaluation.evaluatations)
